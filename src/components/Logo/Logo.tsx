551db96b5a6ac9a55ce3678d71651dbb0bbcadc1
XcQuid <creed.35@yandex.ru>
2022-09-29 14:08:12 +0300

    feat: add Logo

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Logo.scss';
+
+const Logo: React.FC = () => {
+  return (
+    <div className='logo'>
+      <div className='logo__left' />
+      <div className='logo__right' />
+    </div>
+  )
+};
+
+export default Logo;
