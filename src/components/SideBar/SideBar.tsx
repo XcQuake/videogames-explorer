0b312a64ea0c0abb805bfeb19f50cd035abe3822
XcQuid <creed.35@yandex.ru>
2022-10-05 14:07:52 +0300

    fix: change SideBar content

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,19 +1,59 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import Logo from '../Logo/Logo';
 
 import './SideBar.scss';
 
-class SideBar extends React.Component {
+interface Props {};
+
+interface States {
+  isOpen: boolean;
+}
+
+class SideBar extends React.Component<Props, States> {
+  constructor(props: Props) {
+    super(props)
+
+    this.state = {
+      isOpen: false,
+    }
+  }
+
+  private handleOpenMenu(): void {
+    this.setState({isOpen: !this.state.isOpen});
+  }
+
   render() {
     return (
-      <div className='sidebar sidebar_opened'>
-        <nav className='navbar'>
-          <NavLink className='navbar__link' to='/movies'>Popular Searches</NavLink>
-          <NavLink className='navbar__link' to='/tv-shows'>Сериалы</NavLink>
-          <NavLink className='navbar__link' to='/digital-releases'>Цифровые релизы</NavLink>
-          <NavLink className='navbar__link' to='/acters'>Актёры</NavLink>
-        </nav>
-      </div>
+      <>
+        <div className={ `sidebar ${this.state.isOpen ? 'sidebar_opened' : 'sidebar_closed'}` }>
+          <div className='sidebar__header'>
+            <button className='sidebar__button' onClick={() => this.handleOpenMenu()}/>
+            {/* <p className='sidebar__title'>Kino-Movies</p> */}
+          </div>
+          <div className='sidebar__logo-wrapper'>
+            <Logo />
+          </div>
+          <nav className='navbar'>
+            <NavLink className='navbar__link' to='/movies'>
+              <div className='navbar__icon navbar__icon_movies' />
+              <div className='navbar__link-text'>Фильмы</div>
+            </NavLink>
+            <NavLink className='navbar__link' to='/tv-shows'>
+              <div className='navbar__icon navbar__icon_tv-shows' />
+              <span className='navbar__link-text'>Сериалы</span>
+            </NavLink>
+            <NavLink className='navbar__link' to='/digital-releases'>
+              <div className='navbar__icon navbar__icon_digital-releases' />
+              <span className='navbar__link-text'>Цифровые релизы</span>
+            </NavLink>
+            <NavLink className='navbar__link' to='/acters'>
+              <div className='navbar__icon navbar__icon_acters' />
+              <span className='navbar__link-text'>Персоны</span>
+            </NavLink>
+          </nav>
+        </div>
+      </>   
     )
   }
 }
