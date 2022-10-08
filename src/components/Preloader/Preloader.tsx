45b4e169725c8841d3c1a3b292e4c5be0f76018d
XcQuid <creed.35@yandex.ru>
2022-10-08 13:26:04 +0300

    feat: add Preloader

---
+++ src/components/Preloader/Preloader.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './Preloader.scss';
+
+const Preloader: React.FC = () => {
+  return (
+    <div className="preloader">
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  )
+};
+
+export default Preloader;
