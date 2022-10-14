0560cfcf152a69eb0bf38dfc7151a2e1bfdeb508
XcQuid <creed.35@yandex.ru>
2022-10-14 15:19:20 +0300

    feat: change game list grid template

--- src/components/GameCard/GameCard.tsx
+++ src/components/GameCard/GameCard.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
+import placeholder from '../../images/placeholder-image.jpg';
 import { GameResponse } from '../../types/rawgApiTypes';
 import './GameCard.scss';
 
@@ -8,6 +10,8 @@ interface Props {
 }
 
 const GameCard: React.FC<Props> = ({ game }) => {
+  const gameCardRef = useRef<HTMLDivElement>(null);
+  const [spansCount, setSpansCount] = useState(27);
   const renderPlatforms: JSX.Element = (
     <div className='game-card__platforms'>
       {game.parent_platforms.map((platform) => {
@@ -16,17 +20,30 @@ const GameCard: React.FC<Props> = ({ game }) => {
     </div>
   )
 
+  const setSpans = () => {
+    if (!gameCardRef.current?.clientHeight) return;
+    console.log(gameCardRef.current.clientHeight);
+    const cardDescHeight = gameCardRef.current.clientHeight;
+    const spans = Math.ceil(cardDescHeight / 10) + 20;
+    setSpansCount(spans);
+  }
+
+  useEffect(() => {
+    if (gameCardRef.current) setSpans();
+  }, [gameCardRef.current])
+
   return (
-    <li className='game-card'>
+    <li className='game-card' style={{gridRowEnd: `span ${spansCount}`}}>
       <div className='game-card__image'>
-        <img
+        <LazyLoadImage
           className='game-card__poster'
           src={game.short_screenshots[0].image}
-          loading='lazy'
+          width={200}
+          height={180}
           alt='Game Poster'
         />
       </div>
-      <div className='game-card__description'>
+      <div className='game-card__description' ref={gameCardRef}>
         <p className='game-card__name'>
           {game.name}
         </p>
