e1a297d314fae3a280db3132b03b76b51c520310
XcQuid <creed.35@yandex.ru>
2022-10-22 15:07:50 +0300

    feat: add month changing to calendar & update styles

---
+++ src/components/Calendar/calendarData.ts
@@ -0,0 +1,74 @@
+export const monthsList = [
+  {
+    id: 0,
+    shortName: 'Янв',
+    name: 'Январь',
+    num: 1,
+  },
+  {
+    id: 1,
+    shortName: 'Фев',
+    name: 'Февраль',
+    num: 2,
+  },
+  {
+    id: 2,
+    shortName: 'Мар',
+    name: 'Март',
+    num: 3,
+  },
+  {
+    id: 3,
+    shortName: 'Апр',
+    name: 'Апрель',
+    num: 4,
+  },
+  {
+    id: 4,
+    shortName: 'Май',
+    name: 'Май',
+    num: 5,
+  },
+  {
+    id: 5,
+    shortName: 'Июн',
+    name: 'Июнь',
+    num: 6,
+  },
+  {
+    id: 6,
+    shortName: 'Июл',
+    name: 'Июль',
+    num: 7,
+  },
+  {
+    id: 7,
+    shortName: 'Авг',
+    name: 'Август',
+    num: 8,
+  },
+  {
+    id: 8,
+    shortName: 'Сен',
+    name: 'Сентябрь',
+    num: 9,
+  },
+  {
+    id: 9,
+    shortName: 'Окт',
+    name: 'Октябрь',
+    num: 10,
+  },
+  {
+    id: 10,
+    shortName: 'Ноя',
+    name: 'Ноябрь',
+    num: 11,
+  },
+  {
+    id: 11,
+    shortName: 'Дек',
+    name: 'Декабрь',
+    num: 12,
+  },
+]
