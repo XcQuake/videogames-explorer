e80c6f05c9f3d7398e6e9de83506d1dd3884948f
XcQuid <creed.35@yandex.ru>
2022-10-25 12:11:30 +0300

    style: prettier MonthView component

--- src/components/Calendar/MonthView.tsx
+++ src/components/Calendar/MonthView.tsx
@@ -20,37 +20,52 @@ const MonthView: React.FC = () => {
   };
 
   useEffect(() => {
-    dispatch(setRange(['2022-10-1', '2022-10-23']))
+    dispatch(setRange(['2022-10-1', '2022-10-23']));
   }, []);
 
   const renderDays = () => {
     const days = [];
-    for (let i = 1; i<=monthLength; i++) {
+    for (let i = 1; i <= monthLength; i++) {
       const stringDate = `${year}-${month}-${i}`; // Дата в виде string
       const fullDate = new Date(stringDate); // Дата в виде Date
 
       // Выбрана ли эта дата
       const isPickedDay =
-        (range[0] === stringDate && !compareAsc(fullDate, pickedDates.early))
-        || (range[1] === stringDate && !compareAsc(fullDate, pickedDates.late));
-      
+        (range[0] === stringDate && !compareAsc(fullDate, pickedDates.early)) ||
+        (range[1] === stringDate && !compareAsc(fullDate, pickedDates.late));
+
       // Находится ли эта в диапозоне выбранных дат
       const inRange =
-        (!isDragged && compareAsc(fullDate, pickedDates.early) > 0) 
-        && (!isDragged && compareAsc(fullDate, pickedDates.late) < 0);
+        !isDragged &&
+        compareAsc(fullDate, pickedDates.early) > 0 &&
+        compareAsc(fullDate, pickedDates.late) < 0;
 
       // Находится ли эта дата в диапозоне между первой выбранной датой и датой, на которую наведена мышь
-      const inRangeWhileDrag = hoverDate &&
-(       ((compareAsc(pickedDates.early, hoverDate) > 0) && (compareAsc(fullDate, hoverDate) > 0) && (compareAsc(fullDate, pickedDates.early) < 0))
-        || ((compareAsc(pickedDates.early, hoverDate) < 0) && (compareAsc(fullDate, hoverDate) < 0) && (compareAsc(fullDate, pickedDates.early) > 0)))
+      const isEarlierThanFirstDay =
+        hoverDate &&
+        compareAsc(pickedDates.early, hoverDate) > 0 &&
+        compareAsc(fullDate, hoverDate) > 0 &&
+        compareAsc(fullDate, pickedDates.early) < 0;
+
+      const isLaterThanFirstDay =
+        hoverDate &&
+        compareAsc(pickedDates.early, hoverDate) < 0 &&
+        compareAsc(fullDate, hoverDate) < 0 &&
+        compareAsc(fullDate, pickedDates.early) > 0;
+
+      const inPreview = isEarlierThanFirstDay || isLaterThanFirstDay;
 
       days.push(
         <li
-          className='calendar__day'
+          className="calendar__day"
           key={`day${i}`}
           style={{
             gridColumnStart: i === 1 ? monthStart : 'auto',
-            backgroundColor: isPickedDay ? '#6B69F9' : inRange || inRangeWhileDrag ? '#6b69f946' : 'inherit',
+            backgroundColor: isPickedDay
+              ? '#6B69F9'
+              : inRange || inPreview
+              ? '#6b69f946'
+              : 'inherit',
           }}
           onClick={() => {
             if (!isDragged) {
@@ -64,26 +79,28 @@ const MonthView: React.FC = () => {
           onMouseEnter={() => {
             isDragged && setHoverDate(fullDate);
           }}
-        >{i}</li>
-      )
-    };
+        >
+          {i}
+        </li>
+      );
+    }
     return days;
   };
 
   return (
-    <div className='calendar__view'>
-      <ul className='calendar__list calendar__list_month'>
-        <li className='calendar__weekday'>п</li>
-        <li className='calendar__weekday'>в</li>
-        <li className='calendar__weekday'>с</li>
-        <li className='calendar__weekday'>ч</li>
-        <li className='calendar__weekday'>п</li>
-        <li className='calendar__weekday'>с</li>
-        <li className='calendar__weekday'>в</li>
+    <div className="calendar__view">
+      <ul className="calendar__list calendar__list_month">
+        <li className="calendar__weekday">п</li>
+        <li className="calendar__weekday">в</li>
+        <li className="calendar__weekday">с</li>
+        <li className="calendar__weekday">ч</li>
+        <li className="calendar__weekday">п</li>
+        <li className="calendar__weekday">с</li>
+        <li className="calendar__weekday">в</li>
         {renderDays()}
       </ul>
     </div>
-  )
+  );
 };
 
 export default MonthView;
