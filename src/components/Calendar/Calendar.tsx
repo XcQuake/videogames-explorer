bd2a4f1029ee6a0d34813979b5a088ae17b05f19
XcQuid <creed.35@yandex.ru>
2022-10-26 20:01:28 +0300

    feat: add DecadeView to calendar

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -5,17 +5,17 @@ import { useAppSelector } from '../../hooks/redux-hoos';
 import MonthView from './MonthView';
 import CalendarNavbar from './CalendarNavbar';
 import YearView from './YearView';
-import MenuBar from './CalendarMenuBar';
+import DecadeView from './DecadeView';
 
 const Calendar: React.FC = () => {
   const { view } = useAppSelector((state) => state.calendar);
 
   return (
     <div className="calendar">
-      <MenuBar />
       <CalendarNavbar />
       {view === 'month' && <MonthView />}
       {view === 'year' && <YearView />}
+      {view === 'decade' && <DecadeView />}
     </div>
   );
 };
---
+++ src/components/Calendar/DecadeView.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { getDecade, startOfDecade, compareAsc } from 'date-fns';
+
+import { useAppDispatch, useAppSelector } from '../../hooks/redux-hoos';
+import { setRangeValue } from '../../state/calendarState';
+import DateListElement from './DateListElement';
+
+const DecadeView: React.FC = () => {
+  const { decadeStart } = useAppSelector((state) => state.calendar);
+  const [isPreviewShown, setIsPreviewShown] = useState(false);
+  const [hoverDate, setHoverDate] = useState<Date | null>(null);
+  const dispatch = useAppDispatch();
+
+  const renderYears = () => {
+    function handleClick(stringDate: string) {
+      if (!isPreviewShown) {
+        setIsPreviewShown(true);
+        dispatch(setRangeValue(stringDate));
+      } else {
+        dispatch(setRangeValue(stringDate));
+        setIsPreviewShown(false);
+      }
+    }
+
+    function handleMouseEnter(fullDate: Date) {
+      isPreviewShown && setHoverDate(fullDate);
+    }
+
+    const years = [];
+    for (let i = decadeStart; i < decadeStart + 10; i++) {
+      const stringDate = `${i}-01-01`;
+      years.push(
+        <DateListElement
+          key={`year${i}`}
+          stringDate={stringDate}
+          inRangeDateStyle={{ backgroundColor: '#6b69f946' }}
+          pickedDateStyle={{ backgroundColor: '#6B69F9' }}
+          previewStyle={{ backgroundColor: '#6b69f946' }}
+          isPreviewShown={isPreviewShown}
+          type="year"
+          onClick={() => handleClick(stringDate)}
+          onMouseEnter={() => handleMouseEnter(new Date(stringDate))}
+          hoverDateOnPreview={hoverDate}
+        />
+      );
+    }
+    return years;
+  };
+
+  return (
+    <div className="calendar__view">
+      <ul className="calendar__list calendar__list_decade">{renderYears()}</ul>
+    </div>
+  );
+};
+
+export default DecadeView;
