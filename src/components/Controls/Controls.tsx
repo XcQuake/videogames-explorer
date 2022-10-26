6b2f1d1e1b06caaf34ba70408988f833c73cdefa
XcQuid <creed.35@yandex.ru>
2022-10-26 19:47:12 +0300

    feat: add Controls component

---
+++ src/components/Controls/Controls.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Controls.scss';
+import DateControls from './DatesControls';
+
+const Controls: React.FC = () => {
+  return (
+    <section className="controls">
+    </section>
+  );
+};
+
+export default Controls;
