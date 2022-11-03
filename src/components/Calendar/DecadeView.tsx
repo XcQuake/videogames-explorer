4d3dda1b6877c1734a9f565f31ff172749ccc2db
XcQuid <creed.35@yandex.ru>
2022-11-03 15:18:45 +0300

    refactor: delte CalendarNavbar component

--- src/components/Calendar/DecadeView.tsx
+++ src/components/Calendar/DecadeView.tsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { useAppDispatch, useAppSelector } from '../../hooks/redux-hoos';
 import { setYear } from '../../state/calendarState';
+
 import DateListElement from './DateListElement';
+import Button from '../Button/Button';
+import Icon from '../Icon/Icon';
 
 interface Props {
   onSelect: (date: number) => void;
 }
 
 const DecadeView: React.FC<Props> = ({ onSelect }) => {
-  const { decadeStart } = useAppSelector((state) => state.calendar);
+  const [decade, setDecade] = useState(2020);
   const dispatch = useAppDispatch();
+
+  const title = `${decade} - ${decade + 9}`;
+
+  function handleClickPrevButton() {
+    setDecade(decade - 10);
+  }
+
+  function handleClickNextButton() {
+    setDecade(decade + 10);
+  }
 
   function handleSelectDate(year: number) {
     dispatch(setYear(year));
@@ -19,7 +32,7 @@ const DecadeView: React.FC<Props> = ({ onSelect }) => {
 
   const renderYears = () => {
     const years = [];
-    for (let i = decadeStart; i < decadeStart + 10; i++) {
+    for (let i = decade; i < decade + 10; i++) {
       years.push(
         <DateListElement
           key={`year${i}`}
@@ -32,9 +45,32 @@ const DecadeView: React.FC<Props> = ({ onSelect }) => {
   };
 
   return (
-    <div className="calendar__view">
-      <ul className="calendar__list calendar__list_decade">{renderYears()}</ul>
-    </div>
+    <>
+      <div className="calendar-navbar">
+        <Button
+          size="small"
+          color="inherit"
+          onClick={() => handleClickPrevButton()}
+        >
+          <Icon color="black" name="arrow_left" />
+        </Button>
+        <Button size="small" color="inherit">
+          {title}
+        </Button>
+        <Button
+          size="small"
+          color="inherit"
+          onClick={() => handleClickNextButton()}
+        >
+          <Icon color="black" name="arrow_right" />
+        </Button>
+      </div>
+      <div className="calendar__view">
+        <ul className="calendar__list calendar__list_decade">
+          {renderYears()}
+        </ul>
+      </div>
+    </>
   );
 };
 
