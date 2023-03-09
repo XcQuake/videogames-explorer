da9d0278671746a0d0cfc83c112595d13c6d70af
XcQuid <creed.35@yandex.ru>
2023-03-09 14:27:44 +0300

    refactor: remove back button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
 import SearchBar from '../SearchBar/SearchBar';
 
 import './Header.scss';
-import { Button, Icon } from '../UI';
 
 const Header: React.FC = () => {
-  const history = useHistory();
-
   return (
     <header className="header">
-      <Button onClick={() => history.goBack()} color="inherit" size="tiny">
-        <Icon color="white" name="arrow_left" size="large" />
-      </Button>
       <SearchBar />
     </header>
   );
