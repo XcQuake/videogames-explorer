152c5be95ca33901444885982f5eb4d801faa3f0
XcQuid <creed.35@yandex.ru>
2023-03-09 14:27:20 +0300

    feat: add longarrow icons

--- src/types/index.ts
+++ src/types/index.ts
@@ -13,14 +13,17 @@ export type ICONS =
   | 'arrow_down'
   | 'arrow_right'
   | 'arrow_left'
+  | 'longarrow_left'
+  | 'longarrow_right'
   | 'plus'
   | 'plus_circle'
   | 'options';
-export type SIZES = 'mini' | 'tiny' | 'small' | 'medium' | 'large';
+export type SIZES = 'mini' | 'tiny' | 'small' | 'medium' | 'big' | 'large';
 export type COLORS =
   | 'inherit'
   | 'primary'
   | 'secondary'
   | 'tertiary'
+  | 'quart'
   | 'black'
   | 'white';
