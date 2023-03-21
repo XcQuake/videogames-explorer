65d9e370cf8737707beeeb97f1f98f053a688135
XcQuid <creed.35@yandex.ru>
2023-03-21 10:38:52 +0300

    refactor: simplify SideBar component

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -2,59 +2,23 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { URLS } from '../../utils/contants';
 import Icon from '../UI/Icon/Icon';
-import Logo from '../Logo/Logo';
 
 import './SideBar.scss';
 
-interface Props {}
-
-interface States {
-  isOpen: boolean;
-}
-
-class SideBar extends React.Component<Props, States> {
-  constructor(props: Props) {
-    super(props);
-
-    this.state = {
-      isOpen: false,
-    };
-  }
-
-  private handleOpenMenu(): void {
-    this.setState({ isOpen: !this.state.isOpen });
-  }
-
+class SideBar extends React.Component {
   render() {
     return (
       <>
-        <div
-          className={`sidebar ${
-            this.state.isOpen ? 'sidebar_opened' : 'sidebar_closed'
-          }`}
-        >
-          <div className="sidebar__header">
-            <button
-              className="sidebar__button"
-              onClick={() => this.handleOpenMenu()}
-            />
-            <p className="sidebar__title">Videogames</p>
-          </div>
-          <div className="sidebar__logo-wrapper">
-            <Logo />
-          </div>
+        <div className="sidebar">
           <nav className="navbar">
             <NavLink className="navbar__link" to={URLS.pc}>
               <Icon name="pc" color="secondary" />
-              <span className="navbar__link-text">PC</span>
             </NavLink>
             <NavLink className="navbar__link" to={URLS.playstation}>
               <Icon name="playstation" color="secondary" />
-              <span className="navbar__link-text">PlayStation</span>
             </NavLink>
             <NavLink className="navbar__link" to={URLS.xbox}>
               <Icon name="xbox" color="secondary" />
-              <span className="navbar__link-text">Xbox</span>
             </NavLink>
           </nav>
         </div>
